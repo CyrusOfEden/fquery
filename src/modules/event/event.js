9ed75be78d08cc2dc684968d921923a9c6d60f63
Kash Nouroozi <knouroozi@gmail.com>
2014-12-18 10:44:00 -0500

    Updating events api.

--- src/modules/event/event.js
+++ src/modules/event/event.js
@@ -1,16 +1,16 @@
-query.createEvent = _.curry(function(name, data) {
-  return { name: name, data: data };
+query.buildEventCallback = _.curry(function(callback, node, event) {
+  return callback(node, event);
 });
 
 query.watch = _.curry(function(event, callback, node) {
   node = query.node(node);
-  query.unwrap(node).addEventListener(event, callback);
+  query.unwrap(node).addEventListener(event, query.buildEventCallback(callback, node));
   return node;
 });
 
 query.unwatch = _.curry(function(event, callback, node) {
   node = query.node(node);
-  query.unwrap(node).removeEventListener(event, callback);
+  query.unwrap(node).removeEventListener(event, query.buildEventCallback(callback, node));
   return node;
 });
 
