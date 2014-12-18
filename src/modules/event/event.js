66ea60daeb55570275d10e31882ecc8c58e5509e
Kash Nouroozi <knouroozi@gmail.com>
2014-12-18 14:06:52 -0500

    Fixed query.ready function and added query.trigger.

--- src/modules/event/event.js
+++ src/modules/event/event.js
@@ -2,13 +2,21 @@ query.buildEventCallback = _.curry(function(callback, node, event) {
   return callback(node, event);
 });
 
-query.watch = _.curry(function(event, callback, node) {
+query.watch = _.curry(function(eventNmae, callback, node) {
   node = query.node(node);
   callback = query.buildEventCallback(callback, node);
-  query.unwrap(node).addEventListener(event, callback);
+  query.unwrap(node).addEventListener(eventName, callback);
   return function() {
-    return query.unwrap(node).removeEventListener(event, callback);
+    return query.unwrap(node).removeEventListener(eventName, callback);
   };
 });
 
-query.ready = _.partial(document.addEventListener)('DOMContentLoaded');
+query.trigger = _.curry(function(eventName, node) {
+  node = query.node(node);
+  var event = document.createEvent('HTMLEvents');
+  event.initEvent(eventName, true, false);
+  query.unwrap(node).dispatchEvent(event);
+  return node;
+});
+
+query.ready = _.curry(document.addEventListener)('DOMContentLoaded');
