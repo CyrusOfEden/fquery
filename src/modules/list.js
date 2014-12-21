a9b39ed82ee03d4eb3c27c58bca91644babdf937
Kash Nouroozi <knouroozi@gmail.com>
2014-12-21 09:16:55 -0500

    Removed function call to valueOf, instead get value directly.

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,19 +1,19 @@
 f.list = function(s) {
   if (_.isArray(s)) {
-    this.list = s;
+    this.value = s;
   } else if (s instanceof HTMLCollection || s instanceof NodeList) {
-    this.list = _.toArray(s);
+    this.value = _.toArray(s);
   } else {
-    this.list = _.toArray(document.querySelectorAll(s));
+    this.value = _.toArray(document.querySelectorAll(s));
   }
 };
 
 f.list.prototype = {
   valueOf: function() {
-    return this.list;
+    return this.value;
   }
 };
 
 f.l = function(s) {
-  return new f.list(s).valueOf();
+  return new f.list(s).value;
 };
--- src/modules/node.js
+++ src/modules/node.js
@@ -1,13 +1,13 @@
 f.node = function(s) {
-  this.node = (s instanceof Element || s instanceof Text) ? s : document.querySelector(s);
+  this.value = (s instanceof Element || s instanceof Text) ? s : document.querySelector(s);
 };
 
 f.node.prototype = {
   valueOf: function() {
-    return this.node;
+    return this.value;
   }
 };
 
 f.n = function(s) {
-  return new f.node(s).valueOf();
+  return new f.node(s).value;
 };
