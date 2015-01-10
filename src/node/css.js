b743d7ad1f6ca0f4d72695eacbc7f3c9d68ca1dd
Kash Nouroozi <knouroozi@gmail.com>
2015-01-09 19:08:40 -0500

    Updates to css.js

--- src/node/css.js
+++ src/node/css.js
@@ -1,21 +1,23 @@
-// Get the computed styling of a node.
 /**
- * @param {String} property - the CSS property
+ * Get the computed styling of a node.
+ *
+ * @param {String} prop - the CSS property to fetch
  * @param {Element} node - the node
  * @returns {Any} the value of the node's CSS property
  */
-n.getStyle = function(property, node) {
-  return getComputedStyle(node)[property];
+n.getStyle = function(prop, node) {
+  return getComputedStyle(node)[prop];
 };
 
-// Set the CSS style of a node.
 /**
- * @param {String} property - the CSS property
- * @param {Any} value - the new value of the CSS property
+ * Set the CSS style of a node.
+ *
+ * @param {String} prop - the property to modify
+ * @param {Any} value - the value to set
  * @param {Element} node - the node
  * @returns {Element} the node
  */
-n.setStyle = function(property, value, node) {
-  node.style[property] = get(value, node);
+n.setStyle = function(prop, value, node) {
+  node.style[prop] = get(value, node);
   return node;
 };
